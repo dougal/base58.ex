6f36439508296583e29abfe76e3a44357e8e0b0d
Douglas F Shearer <dougal.s@gmail.com>
2017-03-25 12:30:17 +0000

    Add credo and exdoc as dev dependencies.

--- mix.exs
+++ mix.exs
@@ -7,27 +7,26 @@ defmodule Base58.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     docs: docs()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: []]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:credo, "~> 0.7",   only: :dev},
+      {:ex_doc, "~> 0.13", only: :dev}
+    ]
+  end
+
+  defp docs do
+    [
+      main:       "readme",
+      extras:     ["README.md"]
+    ]
   end
 end
